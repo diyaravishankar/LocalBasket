e00c552248b65d468059bdf49dc52db7fe786394
Mohit Kumar <mkd1112002@gmail.com>
2025-09-11 09:43:54 +0530

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://the-local-basket.vercel.app'
+  site: 'https://the-local-basket.vercel.app',
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
